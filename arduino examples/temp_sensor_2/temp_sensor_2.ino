5b549e79c6a552f3adb957fd9b6e130348c377b0
Basel Hassan <36247274+Basel1Hassan@users.noreply.github.com>
2018-02-21 03:02:29 +0200

    Add files via upload

---
+++ arduino examples/temp_sensor_2/temp_sensor_2.ino
@@ -0,0 +1,15 @@
+ int val;
+ int tempPin = 1; //A1
+ void setup()
+ {Serial.begin(9600);}
+ void loop(){
+  val = analogRead(tempPin);
+float mv = (val*5000/1024);
+float cel = mv*10;
+Serial.print("TEMPERATURE = ");
+Serial.print(cel);
+Serial.print("*C");
+Serial.println();
+delay(1000);
+ }
+
