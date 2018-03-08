cdf17cdb20e2bdc7ed3f4d3f3814d17b9130388f
Basel Hassan <36247274+Basel1Hassan@users.noreply.github.com>
2018-03-08 18:51:16 +0200

    Add files via upload
    the issue of moving left and right is solved , no need for PWM method

--- Code/Motor_Code/Motor_Code.ino
+++ Code/Motor_Code/Motor_Code.ino
@@ -6,8 +6,10 @@ int l3=6;
 int r3=7;
 int l4=8;
 int r4=9;
+int mode = 'g';
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
 pinMode(l1,OUTPUT);
 pinMode(r1,OUTPUT);
 pinMode(l2,OUTPUT);
@@ -38,21 +40,21 @@ void forward(){
         digitalWrite(r4,0);
   }
   void left(){
-  digitalWrite(l1,1);
-    digitalWrite(r1,0);
-      digitalWrite(l2,1);
-        digitalWrite(r2,0);
+ digitalWrite(l1,0);
+    digitalWrite(r1,1);
+      digitalWrite(l2,0);
+        digitalWrite(r2,1);
          digitalWrite(l3,1);
     digitalWrite(r3,0);
       digitalWrite(l4,1);
         digitalWrite(r4,0);
   }
   void right(){
-  digitalWrite(l1,0);
-    digitalWrite(r1,1);
-      digitalWrite(l2,0);
-        digitalWrite(r2,1);
-        digitalWrite(l3,0);
+ digitalWrite(l1,1);
+    digitalWrite(r1,0);
+      digitalWrite(l2,1);
+        digitalWrite(r2,0);
+         digitalWrite(l3,0);
     digitalWrite(r3,1);
       digitalWrite(l4,0);
         digitalWrite(r4,1);
@@ -67,8 +69,44 @@ void forward(){
       digitalWrite(l4,0);
         digitalWrite(r4,0);
   }
+  /*void test(){
+  digitalWrite(l1,0);
+    digitalWrite(r1,1);
+      digitalWrite(l2,0);
+        digitalWrite(r2,1);
+         digitalWrite(l3,1);
+    digitalWrite(r3,0);
+      digitalWrite(l4,1);
+        digitalWrite(r4,0);
+  }*/
 void loop() {
   // put your main code here, to run repeatedly:
 //put the functions above here after using bluetooth
+//test();
+if(Serial.available()>0){ 
+mode = Serial.read();
+} 
+if(mode=='a'){ // Forward
+  Serial.println(mode);
+forward();
+}
+if(mode=='d'){ // backward
+  Serial.println(mode);
+backward();
+}
+if(mode=='b'){ // right
+  Serial.println(mode);
+right();
+}
+if(mode=='e'){ // left
+  Serial.println(mode);
+left();
+}
+if(mode=='c'){ // stop
+  Serial.println(mode);
+stoop();
+}
+
+
 
 }
