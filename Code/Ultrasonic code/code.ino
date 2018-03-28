3191bd06f738a009e64e3cfd67054ff6ddaff644
ehababdelghany <36567468+ehababdelghany@users.noreply.github.com>
2018-03-28 22:44:34 +0200

    Update code.ino

--- Code/Ultrasonic code/code.ino
+++ Code/Ultrasonic code/code.ino
@@ -1,1 +1,17 @@
 
+code: digitalWrite(trig,LOW);
+delayMicroseconds(2);
+digitalWrite (trig,HIGH);
+delayMicroseconds(10);
+digitalWrite (trig,LOW);
+
+duration = pulseIn(echo,HIGH);
+distance = (duration/2)/28.5;
+Serial.println(distance);
+/*Serial.println(distance);
+if(distance<20){stoop();}
+*/
+
+if(distance<6){stoop(); delay(500);
+right(); delay(250);
+stoop(); delay(250);goto code;}
