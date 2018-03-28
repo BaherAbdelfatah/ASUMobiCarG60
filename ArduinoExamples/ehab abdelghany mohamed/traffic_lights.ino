2d18d24a928dd795440e3621aa58990058e5c2eb
ehababdelghany <36567468+ehababdelghany@users.noreply.github.com>
2018-03-28 22:48:09 +0200

    Add files via upload

---
+++ ArduinoExamples/ehab abdelghany mohamed/traffic_lights.ino
@@ -0,0 +1,130 @@
+void setup() {
+  // initialize digital pin 13 as an output.
+ pinMode(0, OUTPUT);
+ pinMode(1, OUTPUT);
+ pinMode(2, OUTPUT);
+ pinMode(3, OUTPUT);
+ pinMode(4, OUTPUT);
+ pinMode(5, OUTPUT);
+ pinMode(6, OUTPUT);
+ pinMode(7, OUTPUT);
+ pinMode(8, OUTPUT);
+ pinMode(9, OUTPUT);
+ pinMode(10, OUTPUT);
+ pinMode(11, OUTPUT);
+}
+
+// the loop function runs over and over again forever
+void loop() {
+  digitalWrite(0, HIGH);  
+  delay(15000);             
+  digitalWrite(0, LOW);   
+  delay(500);   
+
+           
+ digitalWrite(2, HIGH);  
+  delay(15000);             
+  digitalWrite(2, LOW);   
+  delay(500); 
+                        
+
+ digitalWrite(1, HIGH);  
+  delay(750);             
+  digitalWrite(1, LOW);   
+  delay(750); 
+                        
+ digitalWrite(1, HIGH);  
+  delay(750);             
+  digitalWrite(1, LOW);   
+  delay(750); 
+                        
+ digitalWrite(1, HIGH);  
+  delay(750);             
+  digitalWrite(1, LOW);   
+  delay(750); 
+             
+
+ digitalWrite(5, HIGH);  
+  delay(15000);             
+  digitalWrite(5, LOW);   
+  delay(500);   
+
+           
+ digitalWrite(3, HIGH);  
+  delay(15000);             
+  digitalWrite(3, LOW);   
+  delay(500); 
+                        
+
+ digitalWrite(4, HIGH);  
+  delay(750);             
+  digitalWrite(4, LOW);   
+  delay(750); 
+                        
+ digitalWrite(4, HIGH);  
+  delay(750);             
+  digitalWrite(4, LOW);   
+  delay(750); 
+                        
+ digitalWrite(4, HIGH);  
+  delay(750);             
+  digitalWrite(4, LOW);   
+  delay(750); 
+
+
+ digitalWrite(11, HIGH);  
+  delay(15000);             
+  digitalWrite(11, LOW);   
+  delay(500);   
+
+           
+ digitalWrite(9, HIGH);  
+  delay(15000);             
+  digitalWrite(9, LOW);   
+  delay(500); 
+                        
+
+ digitalWrite(10, HIGH);  
+  delay(750);             
+  digitalWrite(10, LOW);   
+  delay(750); 
+                        
+ digitalWrite(10, HIGH);  
+  delay(750);             
+  digitalWrite(10, LOW);   
+  delay(750); 
+                        
+ digitalWrite(10, HIGH);  
+  delay(750);             
+  digitalWrite(10, LOW);   
+  delay(750); 
+
+
+
+ digitalWrite(8, HIGH);  
+  delay(15000);             
+  digitalWrite(8, LOW);   
+  delay(500);   
+
+           
+ digitalWrite(6, HIGH);  
+  delay(15000);             
+  digitalWrite(6, LOW);   
+  delay(500); 
+                        
+
+ digitalWrite(7, HIGH);  
+  delay(750);             
+  digitalWrite(7, LOW);   
+  delay(750); 
+                        
+ digitalWrite(7, HIGH);  
+  delay(750);             
+  digitalWrite(7, LOW);   
+  delay(750); 
+                        
+ digitalWrite(7, HIGH);  
+  delay(750);             
+  digitalWrite(7, LOW);   
+  delay(750); 
+}
